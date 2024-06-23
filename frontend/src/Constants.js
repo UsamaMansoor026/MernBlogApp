964eadc1e825b8d67f1bd263584cacfd1a8c6247
usama mansoor <maan026@gmail.com>
2024-06-23 15:51:30 +0500

    FrontEnd done related to Posts

---
+++ frontend/src/Constants.js
@@ -0,0 +1,39 @@
+import Thumbnail1 from "./assets/blog1.jpg";
+import Thumbnail2 from "./assets/blog2.jpg";
+import Thumbnail3 from "./assets/blog3.jpg";
+import Thumbnail4 from "./assets/blog4.jpg";
+
+export const DummyPosts = [
+  {
+    id: 1,
+    thumbnail: Thumbnail1,
+    category: "education",
+    title: "This is title of 1st blog",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati.",
+    authorID: 1,
+  },
+  {
+    id: 2,
+    thumbnail: Thumbnail2,
+    category: "technology",
+    title: "This is title of 2nd blog",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati.",
+    authorID: 13,
+  },
+  {
+    id: 3,
+    thumbnail: Thumbnail3,
+    category: "weather",
+    title: "This is title of 3rd blog",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati.",
+    authorID: 2,
+  },
+  {
+    id: 4,
+    thumbnail: Thumbnail4,
+    category: "science",
+    title: "This is title of 4th blog",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit, veniam nulla iste labore aut cumque quos saepe in consequuntur obcaecati.",
+    authorID: 3,
+  },
+];
